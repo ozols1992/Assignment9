4f3be4b9a6570551666ca8ae51bc00605cb3de56
DESKTOP-I36G5N2\ozols <ozols1992@otmail.com>
2017-10-04 12:24:06 +0200

    city class

--- includes/city.php
+++ includes/city.php
@@ -6,7 +6,50 @@
  * Time: 11:02
  */
 
-class city
-{
+class city  {
+
+    private $country;
+    private $district;
+    private $id;
+    private $name;
+    private $population;
+
+
+
+    public function __construct($country, $district, $id, $name, $population ) {
+        $this->country = $country;
+        $this->district = $district;
+        $this->id = $id;
+        $this->name = $name;
+        $this->population = $population;
+
+    }
+
+    public function getCountry() {
+        return $this->country;
+    }
+
+    public function getDistrict() {
+        return $this->district;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPopulation() {
+        return $this->population;
+    }
+
+    public function __toString()
+    {
+        return sprintf("%s, %s, %s, %s, %s\n"
+        ,$this->getCountry(),$this->getdistrict(), $this->getid(), $this->getname(), $this->getpopulation());
+
+    }
 
 }
--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
     /*
-     * This is the application
-     */
+* This is the application
+*/
     session_start();
-    require_once './includes/TelevisionI.inc.php';
-    require_once './includes/Television.inc.php';   // the model
-    require_once './includes/TelevisionV1.inc.php'; // a view
-    require_once './includes/TelevisionRC.inc.php'; // a controller
+
+    require_once './includes/City.php';   // the model
+
 ?>
 <!doctype html>
 <html>
@@ -18,13 +17,10 @@
   <body>       
       <div class="box">
           <?php
-    $model = new Television();          // init the model
-    $rc = new TelevisionRC($model);     // init a controller
-    $view1 = new TelevisionV1($model);  // init a view
-    if (isset($_POST)) {                // did we receive data?
-        $rc->action($_POST);            // the controller deals with it
-    }
-    printf("%s\n", $view1->display0());      // use the view, display state
+          $c = new City('Denmark', 'me', 'DK', 'Kolding', '100' );
+
+          echo $c;
+
     ?>         
       </div>           
   </body>
