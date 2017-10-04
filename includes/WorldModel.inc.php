f5c440a64185837632932574bc0701e715efc2f6
Kevin <kevsmultimedia@gmail.com>
2017-10-04 10:24:25 +0200

    Private classes created

--- includes/WorldModel.inc.php
+++ includes/WorldModel.inc.php
@@ -2,16 +2,19 @@
 /*
  * This is the MODEL
  */
-require_once './includes/DbP.inc.php';
-require_once './includes/DbH.inc.php';
+require_once '../includes/DbP.inc.php';
+require_once '../includes/DbH.inc.php';
 
 class WorldModel implements WorldIf {
-    private $on;      // true if on, otherwise false
-    private $channel; // integer with current channel number
-    private $volume;  // integer with current volume level 
-    private $mute;    // false if not muted, otherwise true
-    private $videos = array();
-    
+    private $district;      // district of cty
+    private $id;            // zip code
+    private $name;          // name of city
+    private $population;    // population for the city
+    private $language;      // Languge of country
+    private $isOfficial;    // Is it official?
+    private $percentage;    // percentage who speaks it.
+    private $countrycode;   //
+
     /*
      * get state from session array
      * normally you'd read input params
@@ -26,70 +29,70 @@ class WorldModel implements WorldIf {
     }
 
     public function getTvOnOff() {
-      return $this->on;
+        return $this->on;
     }
 
     public function tvOnOff() {
-      $this->on = $this->on ? FALSE : TRUE;
-      $this->saveState();
+        $this->on = $this->on ? FALSE : TRUE;
+        $this->saveState();
     }
 
     public function getChannel() {
-      return $this->channel;
+        return $this->channel;
     }
 
     public function chUp() {
-      $this->channel += 1;
-      $this->saveState();
-      $this->populateMedia($this->getChannel(), DbH::getDbH());
+        $this->channel += 1;
+        $this->saveState();
+        $this->populateMedia($this->getChannel(), DbH::getDbH());
     }
 
     public function chDown() {
-      if ($this->channel > 1) {
-          $this->channel -= 1;
-      } else {
-          $this->channel = 1;
-      }
-      $this->saveState();
-      $this->populateMedia($this->getChannel(), DbH::getDbH());
+        if ($this->channel > 1) {
+            $this->channel -= 1;
+        } else {
+            $this->channel = 1;
+        }
+        $this->saveState();
+        $this->populateMedia($this->getChannel(), DbH::getDbH());
     }
 
     public function getVolume() {
-      return $this->volume;
+        return $this->volume;
     }
 
     public function volUp() {
-      $this->volume += 1;
-      $this->saveState();
+        $this->volume += 1;
+        $this->saveState();
     }
 
     public function volDown() {
-      $this->volume -= 1;
-      $this->saveState();
+        $this->volume -= 1;
+        $this->saveState();
     }
 
     public function getMute() {
-      return $this->mute;
+        return $this->mute;
     }
 
     public function mute() {
-      $this->mute = $this->mute ? FALSE : TRUE;
-      $this->saveState();
+        $this->mute = $this->mute ? FALSE : TRUE;
+        $this->saveState();
     }
 
     public function getMedia() {
-      return $this->videos;
+        return $this->videos;
     }
 
     private function populateMedia($ch, $db) {
-      $this->videos = array();
-      $this->videos = Media::setMedia($ch, $db);
+        $this->videos = array();
+        $this->videos = Media::setMedia($ch, $db);
     }
 
     private function saveState() {
-      $_SESSION['on'] = $this->getTvOnOff();
-      $_SESSION['channel'] = $this->getChannel();
-      $_SESSION['volume'] = $this->getVolume();
-      $_SESSION['mute'] = $this->getMute();
+        $_SESSION['on'] = $this->getTvOnOff();
+        $_SESSION['channel'] = $this->getChannel();
+        $_SESSION['volume'] = $this->getVolume();
+        $_SESSION['mute'] = $this->getMute();
     }
 }
