e1291f60a80e91d8207059a816290a7d5f049845
Kevin <kevsmultimedia@gmail.com>
2017-10-04 13:12:41 +0200

    Country has been completed

--- includes/country.php
+++ includes/country.php
@@ -1,12 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Kevin
- * Date: 04-10-2017
- * Time: 11:01
- */
+    require_once '../includes/DbP.inc.php';
+    require_once '../includes/DbH.inc.php';
+    require_once '../includes/WorldIf.inc.php';
+    require_once '../includes/WorldModel.inc.php';
+class country extends WorldModel implements WorldIf {
+    private $code;
 
-class country
-{
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
+    public function getCode(){
+        return $this->code;
+    }
+    public function create() {}
+    public function update() {}
+    public function delete() {}
 
 }
