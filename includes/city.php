79ce9b29befee60ba960d4871b5914353046f713
Kevin <kevsmultimedia@gmail.com>
2017-10-05 09:38:34 +0200

    Updated a new insert sentense

--- includes/city.php
+++ includes/city.php
@@ -43,25 +43,49 @@ class city {
 
 
 
-}
-    $dbh = DbH::getDbH();
-    $sql = "select name";
-    $sql .= " from city";
-    try
-    {
-        $q = $dbh->prepare($sql);
-        $q->execute();
-        print("<table>\n");
-        while ($row = $q->fetch())
-        {
-            printf("</td><td class='name'>%s</td></tr>\n",
-                $row['name']);
-        }
 
-        print("$row\n");
-        } catch
-    (PDOException $e) {
-        printf("<p>%s</p>\n", $e->getMessage());
+
+}
+
+$dbh = DbH::getDbH();
+$sql = "select countrycode ";
+$sql .= " from city";
+$sql .= " where countrycode='ago' ";
+try {
+    $q = $dbh->prepare($sql);
+    $q->execute();
+    print("<table>\n");
+    while ($row = $q->fetch()) {
+        printf("</td><td class='name'>%s</td></tr>\n",
+            $row['countrycode']);
     }
+
+    print("$row\n");
+} catch
+(PDOException $e) {
+    printf("<p>%s</p>\n", $e->getMessage());
+}
+
+$dbh = DbH::getDbH();
+$sql = "select * ";
+$sql .= " from city";
+$sql .= " where countrycode='LVA' ";
+try {
+    $q = $dbh->prepare($sql);
+    $q->execute();
+    print("<table>\n");
+    while ($row = $q->fetch()) {
+        printf("</td><td class='name'>%s</td></tr>\n",
+            $row['countrycode'] . '<br>' .  $row['name'] . '<br>' . $row['district'] . '<br>' . $row['population']);
+    }
+
+    print("$row\n");
+} catch
+(PDOException $e) {
+    printf("<p>%s</p>\n", $e->getMessage());
+}
+
+
+
 
 
